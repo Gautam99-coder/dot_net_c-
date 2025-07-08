13188cd1d66d2b482adea396d02bfabfb9b66bb3
Gautam99-coder <gautamtharu1000@gmail.com>
2025-07-08 15:08:24 +0530

    array foreach loop

---
+++ arr1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gautam_23SOEIT11603
+{
+    internal class arr1
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
