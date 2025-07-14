4bd5637ad2e49778391c1d78a4efe5e3003cf147
Gautam99-coder <gautamtharu1000@gmail.com>
2025-07-14 19:03:12 +0530

    rewrite ,and taking input form user

---
+++ reArrange.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gautam_23SOEIT11603
+{
+    internal class reArrange
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number: ");
+            string str = Console.ReadLine();//taking input from user
+            int x = Convert.ToInt32(str);//converting string to integer
+
+            if (x % 2 == 0)
+                Console.WriteLine("Number is Even");
+            else
+                Console.WriteLine("Number is Odd");
+
+            Console.Read();
+        }
+    }
+}
