aebcd02b25a9baf7a7fccdd4a7aca1fa37ded3b5
Gautam99-coder <gautamtharu1000@gmail.com>
2025-07-17 15:51:34 +0530

    the series 5 +55 + 555 + 5555 + .. n terms.

---
+++ sumOfSerise_5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gautam_23SOEIT11603
+{
+    internal class sumOfSerise_5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of terms: ");
+            int terms = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int term = 0;
+
+            for (int i = 1; i <= terms; i++)
+            {
+                term = term * 10 + num;
+                Console.Write(term);
+                if (i != terms) Console.Write(" + ");
+                sum += term;
+            }
+
+            Console.WriteLine("\nThe Sum is : " + sum);
+        }
+    }
+}
