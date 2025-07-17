3914f547b939ed5c8e8043e66f3d35145c62707f
Gautam99-coder <gautamtharu1000@gmail.com>
2025-07-17 15:49:25 +0530

    print diamond pattern

---
+++ diamondPattern.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gautam_23SOEIT11603
+{
+    internal class diamondPattern
+    {
+        static void Main(string[]arg)
+        {
+            int n = 5; // Diamond height (max width)
+
+            // Upper part
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j < n; j++)
+                    Console.Write(" ");
+                for (int k = 1; k <= (2 * i - 1); k++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            // Lower part
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = n; j > i; j--)
+                    Console.Write(" ");
+                for (int k = 1; k <= (2 * i - 1); k++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
