c7f3ae56774ec6c30128ee4acfcdc1e2d547c8ea
Gautam99-coder <gautamtharu1000@gmail.com>
2025-07-17 15:49:08 +0530

    finding armstrong number checker

---
+++ Armstrong.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gautam_23SOEIT11603
+{
+    internal class Armstrong
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0, temp = number;
+
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += digit * digit * digit;
+                temp /= 10;
+            }
+
+            if (sum == number)
+                Console.WriteLine($"{number} is an Armstrong number.");
+            else
+                Console.WriteLine($"{number} is not an Armstrong number.");
+        }
+    }
+}
