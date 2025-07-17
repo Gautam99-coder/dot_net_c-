a36905d2208ae27cc47259d98db223e230e9d090
Gautam99-coder <gautamtharu1000@gmail.com>
2025-07-17 15:50:16 +0530

    pattern increase in number ,pyramid

---
+++ increaseNum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gautam_23SOEIT11603
+{
+    internal class increaseNum
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j +" ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
