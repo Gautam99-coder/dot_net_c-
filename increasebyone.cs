534ff6c18a1e60b8072f93eeaaa99d91e2d57c7b
Gautam99-coder <gautamtharu1000@gmail.com>
2025-07-17 15:49:52 +0530

    pattern of pyramid in increase by one number

---
+++ increasebyone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gautam_23SOEIT11603
+{
+    internal class increasebyone
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int number = 1;
+            for(int i=1; i<=n; i++)
+            {
+                for(int j=1; j<=i; j++)
+                {
+                    Console.Write(number++ + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
