4a9aec2aeab1396f03d8beebb7f9960b3dfb3b98
Gautam99-coder <gautamtharu1000@gmail.com>
2025-07-17 15:50:55 +0530

    pattern pyramid

---
+++ pyramid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gautam_23SOEIT11603
+{
+    internal class pyramid
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i<=4; i++)
+            {
+                for(int j=1; j<=4+i-1; j++)
+                {
+                    if (j <= 4 - i)
+                        Console.Write(" ");
+                    else if ((j - (4 - i)) % 2 == 1)
+                        Console.Write(j+" ");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
